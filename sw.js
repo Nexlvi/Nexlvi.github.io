d513d123d9d4683bc4310fdc1bfe18715d92ff53
Nexlvi <cnexlvi@gmail.com>
2026-08-07 21:52:48 +0800

    Site updated: 2026-08-07 21:52:47

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1779274515342";const preCache = ["/images/taichi.png","/images/amiya02.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1786110750763";const preCache = ["/images/taichi.png","/images/amiya02.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
